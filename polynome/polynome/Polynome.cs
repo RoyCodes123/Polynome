3686c703b8d4635a1854e59f2df1477ecdad1a67
RoyCodes123 <50462856+RoyCodes123@users.noreply.github.com>
2019-10-28 15:16:13 +0200

    Add files via upload

---
+++ polynome/polynome/Polynome.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Polynome
+    {
+        private double[] poly = new double[10];
+
+        public Polynome(double[] coeffs) 
+        {
+           int count  = 0;
+            for (int i = this.poly.Length - 1; i >= 0; i--) 
+            { 
+                this.poly[i] = coeffs[count];
+                count++;
+            }
+        }
+
+        public void ScalarMul(double s) 
+        {
+            for (int i = 0; i < this.poly.Length; i++) 
+            {
+                this.poly[i] = this.poly[i] * s;
+            }
+        }
+
+        public Polynome() 
+        {
+            for (int i = 0; i < this.poly.Length; i++) 
+            {
+                this.poly[i] = 0;
+            }
+        }
+
+        public Polynome Add(Polynome poly1) 
+        {
+             Polynome sumPoly = new Polynome();
+             for (int i = 0; i < this.poly.Length; i++) 
+             {
+                 sumPoly.poly[i] = poly1.poly[i] + this.poly[i];
+             }
+             return sumPoly;
+        }
+
+        public Polynome Sub(Polynome poly1)
+        {
+            Polynome difPoly = new Polynome();
+            for (int i = 0; i < this.poly.Length; i++)
+            {
+                difPoly.poly[i] = this.poly[i] - poly1.poly[i];
+            }
+            return difPoly;
+        }
+
+        public double GetHighestExpo() {
+            double high = 0;
+
+            for (int i = 1; i < this.poly.Length; i++) 
+            {
+                if (this.poly[i] != 0) {
+                    high = i;
+                }
+            }
+            return high;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            bool flag = true;
+            for (int i = this.poly.Length - 1; i >= 0; i--)
+            {
+                if (this.poly[i] > 0  && flag == true) 
+                {
+                    str += this.poly[i] + "x^" + i + " ";
+                    flag = false;
+                }
+
+                else if (this.poly[i] > 0 )
+                {
+                    str += "+ " + this.poly[i] + "x^" + i + " ";
+                    flag = false;
+                }
+
+                if (this.poly[i] < 0)
+                {
+                    str += this.poly[i] + "x^" + i + " ";
+                    flag = false;
+                }
+                
+            }
+            return str;
+        }
+    }
+}
---
+++ polynome/polynome/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = new double[] { -3, 2, 4.5, 8, -1, 0, 0, 0, -7, 6 };
+            double[] arr1 = new double[] { 0, 2, 5, -6, -1, 0, 7, 0, 0, 1 };
+            Polynome poly = new Polynome(arr);
+            Polynome poly1 = new Polynome(arr1);
+            Console.WriteLine(poly.ToString());
+            Console.WriteLine(poly1.ToString());
+            Console.WriteLine(poly.Add(poly1).ToString());
+        }
+    }
+}
